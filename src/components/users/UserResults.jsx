d3f323ecdfd6e75d2f66abc21d346933ce499ced
Shay Johnson <shay.johnson1@ibm.com>
2022-12-29 10:24:16 -0600

    moved user and isLoading states, plus fetchUsers method to global state with useContext

--- src/components/users/UserResults.jsx
+++ src/components/users/UserResults.jsx
@@ -1,24 +1,14 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect, useContext} from 'react';
+import GithubUserContext from '../../context/GithubUserContext';
 import SpinGif from '../layout/SpinGif';
 import UserItem from './UserItem';
 
 const UserResults = () => {
-    const [users, setUsers] = useState([]);
-    const [isLoading, setIsLoading] = useState(true);
+    const {isLoading, users, fetchUsers} = useContext(GithubUserContext);
     useEffect(() => {
         fetchUsers();
-    },[]);
+    });
 
-    const fetchUsers = async () => {
-        const response = await fetch(process.env.REACT_APP_GITHUB_URL+"/users",{
-            headers:{
-                Authorization:`token ${process.env.REACT_APP_GITHUB_TOKEN}`
-            }
-        });
-        const data = await response.json();
-        setUsers(data);
-        setIsLoading(false);
-    }
     if(!isLoading){
         return (
             <div className='grid grid-cols-1 ga-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2'>
---
+++ src/context/GithubUserContext.js
@@ -0,0 +1,34 @@
+import { createContext, useState } from "react";
+
+const GithubUserContext = createContext();
+
+const GITHUB_URL = process.env.REACT_APP_GITHUB_URL;
+const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN;
+
+export const GithubProvider = ({children}) => {
+    const [users, setUsers] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+
+    const fetchUsers = async () => {
+        const response = await fetch(`${GITHUB_URL}/users`,{
+            headers:{
+                Authorization: `token ${GITHUB_TOKEN}`,
+            }
+        });
+        const data = await response.json()
+        setUsers(data);
+        setIsLoading(false);
+    }
+    return (
+            <GithubUserContext.Provider
+                value={{
+                    users,
+                    isLoading,
+                    fetchUsers
+                }}
+            >
+                {children}
+            </GithubUserContext.Provider>)
+}
+
+export default GithubUserContext;
